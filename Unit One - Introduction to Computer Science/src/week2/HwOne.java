55cf7a21fe90e8d3594becb5835af2c4a94b154a
Arman Momeni <amomeni@bayviewglen.ca>
2021-09-14 15:08:56 -0400

    Homework Unit One

---
+++ Unit One - Introduction to Computer Science/src/week2/HwOne.java
@@ -0,0 +1,97 @@
+package week2;
+
+public class HwOne {
+    public static void main(String[] args) {
+        /* #1
+        double num = 7.23; 
+        double numSqrd = num*num; 
+        System.out.println(num + " * " + num + " = " + numSqrd)
+        */
+
+        /* #2
+        int testFull = 20; 
+        int testResult = 18; 
+        double percent = ((double)testResult/testFull)*100; 
+        System.out.println("your score on the test was: " + percent + "%");
+        */
+
+        /* 3
+        int num = 12345;
+        int num2 = num % 10000 / 1000; 
+        int num4 = num % 100 / 10; 
+        System.out.println(num4 * num2);
+        */ 
+
+        /* 4
+        int vi = 3; 
+        int acc = 5; 
+        int t = 10; 
+        int vf = vi + acc*t;
+        System.out.println("The final vecolity is: " + vf + "m/s");
+        */ 
+
+        /* 5
+        int rad = 2; 
+        final double pi = 3.141592; 
+        double area = pi*(rad*rad);
+        System.out.println(area);
+        */
+
+        /* 6
+        int r = 2; 
+        final double pi = 3.141592; 
+        double volume = 1.3333*pi*(r*r*r);
+        System.out.println("the volume of this sphere is: " + volume);
+        */
+
+        /* 7 
+        double a = 3; 
+        double b = 6; 
+        double c = 8;
+        double x = 5; 
+        double y = a*(x*x)+b*x+c; 
+        System.out.println("Y is equal to: " + y);
+        */ 
+        
+        /* 8
+        double x1 = 2; 
+        double x2 = 4; 
+        double y1 = 5; 
+        double y2 = 6; 
+        double m = (y2-y1)/(x2-x1);
+        System.out.println("The slope of this line is: " + m);
+        */
+
+        /* 9 
+        int numPennies = 2; 
+        int numNickles = 2; 
+        int numDimes = 2; 
+        int numQuarters = 2; 
+        int numLoonies = 2; 
+        int numToonies = 2; 
+
+        double pennyVal = numPennies * 0.01; 
+        double nickleVal = numNickles * 0.05; 
+        double dimeVal = numDimes * 0.1; 
+        double quarterVal = numQuarters * 0.25; 
+        double loonieVal = numLoonies * 1; 
+        double toonieVal = numToonies * 2; 
+
+        double numTotal = pennyVal + nickleVal + dimeVal + quarterVal + loonieVal + toonieVal; 
+
+        System.out.println("Your change is equal to: " + numTotal);
+        */
+
+        /*10
+        double a = 1; 
+        double b = 6; 
+        double c = 1;
+
+        double root1 = (-b + Math.sqrt(b*b - 4*a*c))/2;
+        double root2 = (-b - Math.sqrt(b*b - 4*a*c))/2;
+
+        System.out.println("The roots of this equation are: " + root1 + " and " + root2);
+        */
+       
+    }
+}
