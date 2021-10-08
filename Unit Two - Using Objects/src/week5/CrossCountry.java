3ca6b36eb6757bcc6fa2a1a2bc287f91cf4157b6
Arman Momeni <amomeni@bayviewglen.ca>
2021-10-08 14:06:37 -0400

    Cross Country Poject

--- Unit Two - Using Objects/src/week5/CrossCountry.java
+++ Unit Two - Using Objects/src/week5/CrossCountry.java
@@ -3,6 +3,11 @@ package week5;
 import java.util.Scanner;
 
 public class CrossCountry {
+   /**
+    * main method 
+    * creates scanner object
+    * calls processRunner method 3 times
+    */
    public static void main(String[] args) {
       Scanner in = new Scanner(System.in);
       processRunner(in);
@@ -10,7 +15,14 @@ public class CrossCountry {
       processRunner(in);
       in.close();
    }
-
+   /**
+    * Static method 
+    * No return type - void method
+    * @param in (Type Scanner) Takes user input, and assigns the user's input to a variable 
+    * Declares variables (firstName, lastName, mileOne, mileTwo, finish, splitTwo, splitThree)
+    * calculates splitTwo and splitThree by using the subtractTime method 
+    * Prints out the user's information (Name, Mile one, Split two, Split three, 5km time)
+   */
    private static void processRunner(Scanner in) {
       String firstName, lastName;
       String mileOne, mileTwo, finish;
@@ -45,10 +57,13 @@ public class CrossCountry {
    }
 
    /**
-    * 
-    * @param endTime
-    * @param startTime
-    * @return
+    * @param endTime (string) the first parameter that the method takes, indicates the end time of a certain race period
+    * @param startTime (string) the second parameter that the method takes, indicates the start time of a certain race period
+    * declares variables endTimeInSeconds and startTimeInSeconds - both are type double 
+    * Sets these variables to the number of seconds in the parameter that was given to the method (uses convertToSeconds method)
+    * Subtracts the number of seconds in startTime from endTime 
+    * Converts the difference in seconds to the proper time format (mm:ss.sss) and returns it (uses convertToTime method)
+    * @return return type is string, returns the difference in seconds between the starTime and the endTime (as a string - sequence of characters)
     */
    private static String subtractTime(String endTime, String startTime) {
       double endTimeInSeconds = convertToSeconds(endTime);
@@ -60,6 +75,14 @@ public class CrossCountry {
 
    }
 
+   /**
+    * @param timeInSeconds takes a double value, which is a certain amount of time in seconds and milliseconds
+    * turns from seconds into format (mm:ss.sss)
+    * to get the minutes it gets the timeInSeconds divides it by 60 (60 seconds in a minute) and turns it into an integer value (cutting of the decimal)
+    * to get the seconds it uses the modulus operator, which gives the remainder of dividing the two values
+    * it does (timeInSeconds%60) and then stores the remainder/answer into the seconds variable (type double)
+    * @return returns a string, which formats the minutes and seconds into the proper format (mm:ss.sss)
+    */
    private static String convertToTime(double timeInSeconds) {
       int minutes = (int)(timeInSeconds/60);
       double seconds = (timeInSeconds%60); 
@@ -67,6 +90,16 @@ public class CrossCountry {
       return String.format("%d:%06.3f", minutes, seconds);
    }
 
+   /**
+    * @param time takes a string value (time) 
+    * uses indexOf to find where ":" is in the string
+    * declares two variables untilIndex (a substring from the beginning of the string until the ":") 
+    * and afterIndex (a substring from after the ":" until the end of the string )
+    * declares variable front (type Integer), which turns the untilIndex variable from a string to an integer
+    * multiplies front by 60 so that it turns from minutes to seconds
+    * declares variable back (type Double), which will turn the afterIndex variable from a string to a double 
+    * @return returns front + back, which results in the total number of seconds in the original time that was given as a parameter 
+    */
    private static double convertToSeconds(String time) {  
     int index = time.indexOf(":"); 
     String untilIndex = time.substring(0, index); 
@@ -78,6 +111,8 @@ public class CrossCountry {
     
     return front + back;
    }
+
+   //final copy 
    
 
 }
